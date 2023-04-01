464dff2f79ede7367a72e064b3691152a7cd9988
fikriots <dzakiyul89@gmail.com>
2023-04-02 01:48:08 +0700

    portofolio

---
+++ script.js
@@ -0,0 +1,11 @@
+const header = document.querySelector("header");
+window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 100);
+});
+Let menu = document.querySelector('#menu-icon');
+Let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+}
